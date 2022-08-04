02c2c5fce76b0c41ad8309720aac87ba013f8b1d
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-08-05 01:20:51 +0900

    [Bronze V] Title: 사파리월드, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        bw.write("" + Math.abs(n-m));
+
+        br.close();
+        bw.close();
+    }
+}
