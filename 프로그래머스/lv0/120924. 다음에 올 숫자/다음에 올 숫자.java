990ca907d3b52b045422d5f0d381a8ae425571f4
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-11-30 10:46:36 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {
+        return caculator(common);
+    }
+    
+
+    public int caculator(int[] common) {        
+        if (common[1] - common[0] == common[2] - common[1]) {
+            return common[common.length - 1] + common[1] - common[0];
+        }
+        return common[common.length - 1] * common[1] / common[0];
+    }
+}
