5a2fe38aeffe002218b81e0ddf0f1772652e2c70
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-12-26 18:57:11 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1936 ms, Memory: 89628 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
+            arr[i] = input;
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        for (int[] point : arr) {
+            System.out.println(point[0] + " " + point[1]);
+        }
+    }
+}
