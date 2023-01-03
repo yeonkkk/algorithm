096a01674e0d013ba070968120bcd04ec03388ee
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2023-01-04 01:00:00 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.70 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.split(""))
+                .map(String::toLowerCase)
+                .sorted()
+                .collect(Collectors.joining());
+    }
+}
