51e1825ed5f8dd2bb6e68331dda5df4fde83aa70
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2023-11-07 12:48:13 +0900

    [Silver II] Title: 싸이버개강총회, Time: 156 ms, Memory: 43412 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
@@ -4,19 +4,19 @@ input = sys.stdin.readline
 start, end, stm = input().split()
 
 init_time = 0
-start = int(start[:2] + start[3:])
-end = int(end[:2] + end[3:])
-stm = int(stm[:2] + stm[3:])
+start = int(start.replace(':', ''))
+end = int(end.replace(':', ''))
+stm = int(stm.replace(':', ''))
 
 pre = set()
 post = set()
 while True:
     try:
         time, name = input().split()
-        time = int(time[:2] + time[3:])
+        time = int(time.replace(':', ''))
         
         if init_time <= time <= start: pre.add(name)
-        elif end <= time <=stm: post.add(name)
+        elif end <= time <= stm: post.add(name)
     except:
         break
 
