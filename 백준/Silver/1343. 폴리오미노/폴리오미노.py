b5b4878ff6e5a0df1ded171f6385076662e6dbe7
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2024-01-19 21:15:51 +0900

    [Silver V] Title: 폴리오미노, Time: 136 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+input_str = input()
+
+count = 0
+result = ''
+
+for c in input_str:
+    if c == 'X':
+        count += 1
+        continue
+    else:
+        if count % 2 != 0:
+            result = -1
+            break
+        
+        if count == 0:
+            result += c
+            continue
+
+        result += "AAAA" * (count // 4) + "BB" * ((count % 4) // 2)
+        result += c
+        count = 0
+        
+print(result)
