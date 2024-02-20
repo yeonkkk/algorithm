a44c4321424e972505d0e73446ccd619c43d1ed9
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2024-02-20 17:49:07 +0900

    [Silver IV] Title: 배열 전체 탐색하기, Time: 648 ms, Memory: 44980 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.py
+++ 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.py
@@ -1,5 +1,30 @@
 import sys
 
+def find_index(arr, target):
+    start, end = -1, len(arr)
+
+    while start + 1 < end:
+        mid = (start + end) // 2
+
+        if target <= arr[mid]:
+            end = mid
+        else:
+            start = mid
+
+    return end
+
+def find_last_index(arr, target):
+    start, end = -1, len(arr)
+
+    while start + 1 < end:
+        mid = (start + end) // 2
+
+        if arr[mid] <= target:
+            start = mid
+        else:
+            end = mid
+
+    return end
 
 n, m = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
@@ -8,25 +33,14 @@ arr.sort()
 for _ in range(m):
     arr2 = list(map(int, sys.stdin.readline().split()))
     query_type = arr2[0]
-    count = 0
 
     if query_type == 1:
-        target = arr2[1]
-        for num in arr:
-            if target <= num:
-                count += 1
-    
-    if query_type == 2:
-        target = arr2[1]
-        for num in arr:
-            if target < num:
-                count += 1
-
-    if query_type == 3:
-        target1 = arr2[1]
-        target2 = arr2[2]
-        for num in arr:
-            if target1 <= num <= target2:
-                count += 1
-    
-    print(count)
+        idx = find_index(arr, arr2[1])
+        print(n-idx)
+    elif query_type == 2:
+        idx = find_index(arr, arr2[1] + 1)
+        print(n-idx)
+    else:
+        idx1 = find_index(arr, arr2[1])
+        idx2 = find_last_index(arr, arr2[2])
+        print(idx2 - idx1)
