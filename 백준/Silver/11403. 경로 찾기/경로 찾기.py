608385f79bb005af97fe11c9b6a3186e1b77b5ac
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2024-03-04 16:37:46 +0900

    [Silver I] Title: 경로 찾기, Time: 344 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,18 @@
+import sys
+
+'''
+문제: 경로 찾기 (실버 1)
+분류: 최단 경로
+'''
+
+n = int(sys.stdin.readline())
+graph = [sys.stdin.readline().split() for _ in range(n)]
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+                if 0 < int(graph[i][j]) or 1 < int(graph[i][k]) + int(graph[k][j]):
+                     graph[i][j] = '1'
+
+for line in graph:
+     print(" ".join(line))
