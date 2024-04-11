8690dc8d0c68ee9c2a2ebc77ba65c9798cfd5946
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2024-04-11 19:09:13 +0900

    [Gold V] Title: 거의 소수, Time: 2708 ms, Memory: 111368 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -0,0 +1,37 @@
+import sys, math
+
+input = sys.stdin.readline
+
+def find_prime_numbers(b):
+    check_prime_number = [True] * (int(math.sqrt(b)) + 1)
+    check_prime_number[0] = False
+    check_prime_number[1] = False
+
+    for i in range(2, int(math.sqrt(b) + 1)):
+        if check_prime_number[i]:
+            for j in range(2 * i, int(math.sqrt(b)) + 1, i):
+                check_prime_number[j] = False
+    return check_prime_number
+
+def solution(a, b):
+    count = 0
+    check_prime_numbers = find_prime_numbers(b)
+    
+    for i in range(2, len(check_prime_numbers)):
+        if check_prime_numbers[i]:
+            num = i * i
+            while True:
+                if num < a:
+                    num *= i
+                    continue
+
+                if b < num:
+                    break
+
+                count += 1
+                num *= i
+    return count
+
+# ----- 입력 & 출력 -----
+a, b = map(int, input().split())
+print(solution(a, b))
