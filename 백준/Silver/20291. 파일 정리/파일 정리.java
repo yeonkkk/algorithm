76619a1a4cb97f46bdda59983a9d9747c46c3180
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2024-04-24 18:04:58 +0900

    [Silver III] Title: 파일 정리, Time: 1568 ms, Memory: 140728 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(reader.readLine());
+            List<String> fileNames = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                fileNames.add(reader.readLine());
+            }
+
+            System.out.println(solution(fileNames));
+        }
+    }
+
+    private static String solution(List<String> fileNames) {
+        SortedMap<String, Integer> extensionCount = countExtension(fileNames);
+        StringBuilder result = new StringBuilder();
+
+        for (Entry<String, Integer> entry : extensionCount.entrySet()) {
+            result.append(String.format("%s %d%s", entry.getKey(), entry.getValue(), "\n"));
+        }
+
+        return result.toString();
+    }
+
+    private static SortedMap<String, Integer> countExtension(List<String> fileNames) {
+        SortedMap<String, Integer> extensionCount = new TreeMap<>();
+
+        for (String fileName : fileNames) {
+            String extension = fileName.split("\\.")[1];
+            extensionCount.put(extension, extensionCount.getOrDefault(extension, 0) + 1);
+        }
+
+        return extensionCount;
+    }
+}
