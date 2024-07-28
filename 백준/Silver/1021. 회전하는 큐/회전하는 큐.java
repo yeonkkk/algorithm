14d580b8b2407b9167ed18d47ec9c710abd6d83a
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2024-07-28 23:17:37 +0900

    [Silver III] Title: 회전하는 큐, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,53 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-/**
- * 문제: 회전하는 큐
- */
 
 public class Main {
 
+    private static int solution(int n, int[] numbers) {
+        LinkedList<Integer> deque = new LinkedList<>();
+        for (int i = 1; i < n + 1; i++) {
+            deque.offer(i);
+        }
+
+        int count = 0;
+        first: for (int number : numbers) {
+            int index = deque.indexOf(number);
+
+            if (index + 1 <= deque.size() - index) {
+                while (!deque.isEmpty()) {
+                    int current = deque.pollFirst();
+
+                    if (current == number) {
+                        continue first;
+                    }
+
+                    count++;
+                    deque.offerLast(current);
+                }
+            }
+
+            if (index + 1 > deque.size() - index) {
+                while (!deque.isEmpty()) {
+                    int current = deque.pollLast();
+                    count++;
+
+                    if (current == number) {
+                        continue first;
+                    }
+
+                    deque.offerFirst(current);
+                }
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine().split(" ")[0]);
-        int[] targets = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        System.out.println(solution(n, targets));
-    }
+        try (reader) {
+            String[] firstLine = reader.readLine().split(" ");
+            int n = Integer.parseInt(firstLine[0]);
+            int m = Integer.parseInt(firstLine[1]);
 
-    public static int solution(int n, int[] targets) {
-        LinkedList<Integer> deque = IntStream.range(1, n + 1)
-                .boxed()
-                .collect(Collectors.toCollection(LinkedList::new));
-
-        return Arrays.stream(targets)
-                .map(target -> counterByTarget(target, deque))
-                .sum();
-    }
-
-    private static int counterByTarget(int target, LinkedList<Integer> deque) {
-        int count = 0;
-
-        while (!deque.isEmpty()) {
-            if (deque.peek() == target) {
-                deque.pollFirst();
-                return count;
+            String[] secondLine = reader.readLine().split(" ");
+            int[] numbers = new int[secondLine.length];
+            for (int i = 0; i < secondLine.length; i++) {
+                numbers[i] = Integer.parseInt(secondLine[i]);
             }
-            int targetIndex = deque.indexOf(target);
-            int lastIndex = deque.size() - 1;
 
-            if (targetIndex <= lastIndex - targetIndex) {
-                deque.offerLast(deque.pollFirst());
-            } else {
-                deque.offerFirst(deque.pollLast());
-            }
-            count++;
+            System.out.println(solution(n, numbers));
         }
-        return count;
     }
 }
