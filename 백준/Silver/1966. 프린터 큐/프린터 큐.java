25816c06eecac81c68a62885c5eefebd3a3308a5
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2025-02-20 22:18:32 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,47 +1,73 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.*;
 
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine());
 
-        int k = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String[] firstLine = reader.readLine().split(" ");
+            int n = Integer.parseInt(firstLine[0]);
+            int targetIndex = Integer.parseInt(firstLine[1]);
 
-        for (int i = 0; i < k; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int[] numbers = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            System.out.println(printQ(n, m, numbers));
+            String[] secondLine = reader.readLine().split(" ");
+
+            Integer[] numbers = new Integer[n];
+            Queue<Task> queue = new LinkedList<>();
+
+            for (int j = 0; j < n; j++) {
+                int value = Integer.parseInt(secondLine[j]);
+                Task task = new Task(j, value);
+                queue.offer(task);
+                numbers[j] = value;
+            }
+            Arrays.sort(numbers, (a, b) -> b - a);
+
+            System.out.println(solution(targetIndex, numbers, queue));
         }
     }
 
-    private static int printQ(int n, int m, int[] numbers) {
-        int[] sortedNumbers = Arrays.stream(numbers.clone()).sorted().toArray();
-        Queue<Integer> q = new LinkedList<>();
-        int count = 0;
+    private static int solution(int targetIndex, Integer[] numbers, Queue<Task> queue) {
+        for (int i = 0; i < numbers.length; i++) {
+            int currentMaxValue = numbers[i];
 
-        for (int i = 0; i < n; i++) {
-            q.add(i);
-        }
+            while (!queue.isEmpty()) {
+                Task task = queue.poll();
 
-        while (q.contains(m)) {
-            int currentIndex = q.poll();
+                if (task.isSameValue(currentMaxValue)) {
+                    if (task.isSameIndex(targetIndex)) {
+                        return i + 1;
+                    }
+                    break;
+                }
 
-            if (numbers[currentIndex] == sortedNumbers[n - 1]) {
-                n--;
-                count++;
-                continue;
+                queue.offer(task);
             }
-
-            q.add(currentIndex);
         }
 
-        return count;
+        return 0;
+    }
+
+    public static class Task {
+        private int index;
+        private int value;
+
+        public Task(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        public boolean isSameIndex(int other) {
+            return this.index == other;
+        }
+
+        public boolean isSameValue(int other) {
+            return this.value == other;
+        }
     }
 }
+
