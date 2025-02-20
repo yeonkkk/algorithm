a6f7c67d266b350e8b913dd55fd07eb8ea3d1572
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2025-02-20 21:30:07 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,51 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
 
-/**
- * BOJ 9012. 괄호
- */
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
-    /**
-     * t : 입력될 테스트 데이터 수
-     */
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < t; i++) {
-            System.out.println(checkVps(br.readLine()));
+    public static final String FAIL_VPS = "NO";
+    public static final String SUCCESS_VPS = "YES";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine());
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            String ps = reader.readLine();
+            String result = solution(ps);
+
+            answer.append(result).append('\n');
         }
+
+        System.out.println(answer);
     }
 
-    /**
-     * @param input : 테스트 데이터 (괄호 문자열)  
-     * @return : VPS인지 판단한 결과 (YES, NO)
-     * 열리는 괄호면 stack에 넣고 닫히는 괄호면 stack에서 제거하는 방식이다. 
-     * 값을 제거해야해하는데 stack이 비어있거나, 문자열을 모두 확인했는데 stack에 값이 남아았다면 NO를 반환한다.
-     */
-    public static String checkVps(String input) {
-        Stack<Character> stack = new Stack<>();
+    private static String solution(String ps) {
+        Deque<Character> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < input.length(); i++) {
-            char curChar = input.charAt(i);
-
-            switch (curChar) {
-                case '(':
-                    stack.push(curChar);
-                    break;
-                case ')':
-                    if (!stack.isEmpty()) {
-                        stack.pop();
-                        break;
-                    }
-                    return "NO";
+        for (Character current : ps.toCharArray()) {
+            if (current == '(') {
+                stack.addLast(current);
+                continue;
             }
+
+            if (stack.isEmpty()) {
+                return FAIL_VPS;
+            }
+            stack.pollLast();
         }
+
         if (!stack.isEmpty()) {
-            return "NO";
+            return FAIL_VPS;
         }
-        return "YES";
+        return SUCCESS_VPS;
     }
 }
